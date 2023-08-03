da01ab8cd6545dc51e68e778660c3b5ad6c550a4
Weberson Gehard <109994791+webersongehard@users.noreply.github.com>
2023-08-03 20:44:06 -0300

    Update exe2OBI.py
    2 jogadores de tenis

--- exe2OBI.py
+++ exe2OBI.py
@@ -1,9 +1,29 @@
+2. Duplas de tênis
+Quatro amigos combinaram de jogar tênis em duplas. Cada um dos amigos tem um
+nível de jogo, que é representado por um número inteiro: quanto maior o número,
+melhor o nível do jogador.
+Os quatro amigos querem formar as duplas para iniciar o jogo. De forma a tornar o
+jogo mais interessante, eles querem que os níveis dos dois times formados sejam o mais
+próximo possível. O nível de um time é a soma dos níveis dos jogadores do time.
+Embora eles sejam muito bons jogadores de tênis, os quatro amigos não são muito bons
+em algumas outras coisas, como lógica ou matemática. Você pode ajudá-los e encontrar
+a menor diferença possível entre os níveis dos times que podem ser formados?
+Entrada
+A entrada contém quatro linhas, cada linha contendo um inteiro A, B, C e D, indicando
+o nível de jogo dos quatro amigos.
+Saída
+Seu programa deve produzir uma única linha, contendo um único inteiro, a menor
+diferença entre os níveis dos dois times formados.
+Restrições
+• 0 ≤ A ≤ B ≤ C ≤ D ≤ 104
+
+
 a = int(input("Digite o o nível do jogador A: "))
 b = int(input("Digite o o nível do jogador B: "))
 c = int(input("Digite o o nível do jogador C: "))
 d = int(input("Digite o o nível do jogador D: "))
 
-if a <= b <= c <= d <= (10**2):
-    if a == b and c < d :
-        print(d+a - c+b)
-    
+if 0 <= a <= b <= c <= d <= (10**4):
+  print((d+a)-(b+c))
+else:
+    print("Você digitol valores fora do esperado ")
