27dd4769695d5d09a58bf436d09f7d07cf4dc69e
Weberson Gehard <109994791+webersongehard@users.noreply.github.com>
2023-08-17 20:39:25 -0300

    Update soma13a73.py

--- soma13a73.py
+++ soma13a73.py
@@ -1,9 +1,14 @@
 # fazer um programa que calcule e apresente a média aritmética dos números entre 13 e 73
 x = int()
 i = 0
-
+cont = 0
 for i in range(13,74,1):
     x = x + i
-    i = i + 1
-a = x
-print(a/60)
+    cont = cont + 1
+
+print(x/cont)
+
+
+
+
+    
