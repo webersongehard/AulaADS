51864e7363b23a4f3de7d687118b05c0595276a4
Weberson Gehard <109994791+webersongehard@users.noreply.github.com>
2023-08-21 19:59:39 -0300

    Update pesquisadevogais.py

--- pesquisadevogais.py
+++ pesquisadevogais.py
@@ -5,3 +5,18 @@ texto = "Instituto Federal "
 for letra in texto:
     if letra.lower() in vogais:
         print(f'A letra {letra} é uma vogal! ')
+
+
+
+###################################################################
+
+texto = input("Digite um testo: ")
+vogais = "aeiouAEIOU"
+texto_sem_vogais = ""
+
+for letra in texto:
+    if(letra not in vogais):
+        texto_sem_vogais = texto_sem_vogais + letra
+
+print(texto_sem_vogais)
+        
